27054c331cd5ee9ab8d2fccc17d0ef00980c8285
Seasoning3 <100291787+Seasoning3@users.noreply.github.com>
2023-06-12 20:35:26 +0900

    Add files via upload

---
+++ car2.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int motor1PinA = 2;
+int motor1PinB = 3;
+int enablePin = 11;
+
+void setup() {
+  myservo.attach(9);
+  pinMode(motor1PinA, OUTPUT);
+  pinMode(motor1PinB, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  analogWrite(enablePin, 1000);
+}
+
+void move_forward() {
+  digitalWrite(motor1PinA, LOW);
+  digitalWrite(motor1PinB, HIGH);
+}
+
+void move_backward() {
+  digitalWrite(motor1PinA, HIGH);
+  digitalWrite(motor1PinB, LOW);
+}
+
+void wheel_forward() {
+  myservo.write(90);
+}
+
+void wheel_left() {
+  myservo.write(50);
+}
+
+void wheel_right() {
+  myservo.write(130);
+}
+
+void loop() {
+  wheel_forward();
+  move_forward();
+  delay(750);
+  wheel_left();
+  delay(1500);
+}
